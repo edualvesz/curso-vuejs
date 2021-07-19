0e084f8eb95365b356e88d900ae399fe4251fbe4
edualvesz <ea-hendrix@hotmail.com>
2021-07-19 11:58:35 -0300

    Criacao do componente Painel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <example-component></example-component>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Dashboard">
+
+                    Teste de conteúdo
+                        <div class="row">
+                            <div class="col-md-4">
+                                <painel titulo="Conteudo 1">            
+                                    teste de conteudo
+                                </painel>
+                            </div>
+                            
+                        
+                            <div class="col-md-4">
+                                <painel titulo="Conteudo 2">            
+                                    teste de conteudo
+                                </painel>
+                            </div>
+
+                        
+                            <div class="col-md-4">
+                                <painel titulo="Conteudo 3">            
+                                    teste de conteudo
+                                </painel>
+                            </div>
+                        </div>
+                </painel>
+            </div>
+        </div>
+    </div>
 @endsection
